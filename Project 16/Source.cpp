53e4921d774ad1214d3b7c2cf3326e61ad6bb804
Kostanics <100402113+Kostanics@users.noreply.github.com>
2022-10-09 21:39:37 +0300

    Update Source.cpp

--- Project 16/Source.cpp
+++ Project 16/Source.cpp
@@ -1,48 +1,22 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-void sorty(const int n) {
-    int x;
-    int position = 0;
-    int* arr = new int[n];
-    for (int i = 0; i < n; i++){
-        arr[i] = rand() % n; 
-        cout << arr[i] << "  ";
-    }
-    cout << endl;
-    cout << "Find: "; 
-    cin >> x;
-    if (arr[n - 1] != x){
-        arr[n - 1] = x;
-        for (; arr[position] != x; position++);
-        position++;
-    }
-    else{
-        cout << n;
-    }
-    cout << "position: " << position;
-}
-int main()
-{
-    srand(time(NULL));
-    int n;
-    cout << "For 100 elements:" << endl;
-    sorty(n = 100);
-    system("pause");
-
-    cout << "For 1000 elements:" << endl;
-    sorty(n = 1000);
-    system("pause");
-
-    cout << "For 10000 elements:" << endl;
-    sorty(n = 10000);
-    system("pause");
-
-    cout << "For 100000 elements:" << endl;
-    sorty(n = 100000);
-    system("pause");
-
-    cout << "For 1000000 elements:" << endl;
-    sorty(n = 1000000);
-    system("pause");
+int main(){
+	double delta;
+	cout << "enter delta: ";
+	cin >> delta;
+	double x1 = 0, x2 = 0;
+	while (x1 + x2 <= 2 && x1 >= 0 && x2 >= 0) {
+		if (-2 * x1 - 2 * x2 <= -10) {
+			cout << "-2x1 - 2x2 <= -10 = true; x1 = " << x1 << ", x2 = " << x2 << "  task solved" << endl;
+			return 0;
+		}
+		else if (delta > 0.001) {
+			cout << "-2x1 - 2x2 <= -10 = false; x1 = " << x1 << ", x2 = " << x2 << endl;
+		}
+		x1 += delta;
+		x2 += delta;
+	}
+	cout << "the task is unsolvable";
 }
