51abfcad52390680d5969e7a9d2d374e5866def3
Kostanics <100402113+Kostanics@users.noreply.github.com>
2022-10-09 21:24:31 +0300

    Update Source.cpp

--- Project 2/Source.cpp
+++ Project 2/Source.cpp
@@ -1,22 +1,57 @@
 #include <iostream>
-#include <cmath>
+#include <chrono> 
 using namespace std;
+int swapcount = 0, ifcount = 0;
+void sorty(const int n) {
+    int* random = new int[n] {};
+    srand(time(NULL));
+    for (int i = 0; i < n; i++) {
+        random[i] = rand() % 10;
+    }
+    //cout << "massive: " << endl;
+    for (int i = 0; i < n; i++) {
+        //cout << random[i];
+    }
+    cout << endl;
+    auto start = chrono::high_resolution_clock::now();
+    int x = 0;
+    for (int j = n / 2; j > 0; j /= 2) {
+        for (int k = j; k < n; k++) {
+            ifcount++;
+            x = random[k];
+            int l;
+            for (l = k; l >= j && random[l - j] > x; l -= j) {
+                random[l] = random[l - j];
+                swapcount++;
+            }
+            random[l] = x;
+        }
+    }
+    auto end = chrono::high_resolution_clock::now();
+    cout << "sorting time: " << ((chrono::duration_cast<chrono::microseconds>)(end - start)).count() << " (microseconds)"<<endl;
+    cout << "number of permutations: " << ifcount << endl << "number of comparisons: " << swapcount<< endl;
 
-int main() {
-	double delta;
-	cout << "enter delta: ";
-	cin >> delta;
-	double x1 = 0, x2 = 0;
-	while (x1 + x2 <= 2 && x1 >= 0 && x2 >= 0) {
-		if (-2 * x1 - 2 * x2 <= -10) {
-			cout << "-2x1 - 2x2 <= -10 = true; x1 = " << x1 << ", x2 = " << x2 << "  task solved" << endl;
-			return 0;
-		}
-		else if (delta > 0.001) {
-			cout << "-2x1 - 2x2 <= -10 = false; x1 = " << x1 << ", x2 = " << x2 << endl;
-		}
-		x1 += delta;
-		x2 += delta;
-	}
-	cout << "the task is unsolvable";
+    //cout << "sort massive: " << endl;
+    for (int i = 0; i < n; i++) {
+        //cout << random[i];
+    }
+    delete[] random;
+}
+
+int main()
+{
+    cout << "For 100 elements:" << endl;
+    sorty(100);
+    cout << endl;
+    cout << "For 1000 elements:" << endl;
+    sorty(1000);
+    cout << endl;
+    cout << "For 10000 elements:" << endl;
+    sorty(10000);
+    cout << endl;
+    cout << "For 100000 elements:" << endl;
+    sorty(100000);
+    cout << endl;
+    cout << "For 1000000 elements:" << endl;
+    sorty(1000000);
 }
