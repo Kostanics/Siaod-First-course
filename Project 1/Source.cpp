ab8c97880a3665eaf349cb9bf0ed3fa8225425e5
Kostanics <100402113+Kostanics@users.noreply.github.com>
2022-10-09 21:11:32 +0300

    Update Source.cpp

--- Project 1/Source.cpp
+++ Project 1/Source.cpp
@@ -1,74 +1,55 @@
 #include <iostream>
+#include <chrono> 
 using namespace std;
-void Merge(int* A, int first, int last){
-    int start, middle,  final, j;
-    int* mas = new int[last+1];
-    middle = (first + last) / 2; 
-    start = first; 
-    final = middle + 1; 
-    for (j = first; j <= last; j++) {
-        if ((start <= middle) && ((final > last) || (A[start] < A[final]))) {
-            mas[j] = A[start];
-            start++;
+void sorty(const int n) {
+    int* random = new int[n] {};
+    srand(time(NULL));
+    for (int i = 0; i < n; i++) {
+        random[i] = rand() % 10;
+    }
+    //cout << "massive: " << endl;
+    for (int i = 0; i < n; i++) {
+        //cout << random[i];
+    }
+    cout << endl;
+    auto start = chrono::high_resolution_clock::now();
+    unsigned long long comparisonsCount = 0, swapCount = 0;
+    int temp;
+    int x = 0;
+    for (int j = 0; j < n - 1; j++) {
+        for (int k = n - 1; k > j; k--) {
+            comparisonsCount++;
+            if (random[k] < random[k - 1]) {
+                x = random[k - 1];
+                random[k - 1] = random[k];
+                random[k] = x;
+                swapCount += 3;
+            }
         }
-        else {
-            mas[j] = A[final];
-            final++;
-        }
     }
-    for (j = first; j <= last; j++) {
-        A[j] = mas[j];
+    auto finish = chrono::high_resolution_clock::now();
+    cout << "sorting time: " << ((chrono::duration_cast<chrono::microseconds>)(finish - start)).count() << " (microseconds)\nNumber of permutations: " << swapCount << "\nNumber of comparisons: " << comparisonsCount << "\npermutations and comparisons = " << (comparisonsCount + swapCount) << "\n";
+    //cout << "sort massive: " << endl;
+    for (int i = 0; i < n; i++) {
+        //cout << random[i];
     }
-    delete[]mas;
+    delete[] random;
 }
-
-void MergeSort(int* A, int first, int last){
-    if (first < last){
-        MergeSort(A, first, (first + last) / 2); 
-        MergeSort(A, (first + last) / 2 + 1, last); 
-        Merge(A, first, last); 
-    }
-}
-
-void sorty(const int n){
-    int* A = new int[n+1];
-    cout << "not sort:"<<endl;
-    for (int i = 1; i <= n; i++)
-    {
-        A[i] = rand() % 10;
-        cout << A[i];
-    }
-    MergeSort(A, 1, n);
-    cout<< endl << "sort:"<<endl;
-    for (int i = 1; i <= n; i++) {
-        cout << A[i];
-    }
-    delete[]A;
-    }
 
 int main()
 {
-    srand(time(NULL));
-    int n;
     cout << "For 100 elements:" << endl;
-    sorty(n = 100);
-    system("pause");
-
+    sorty(100);
+    cout << endl;
     cout << "For 1000 elements:" << endl;
-    sorty(n = 1000);
-    system("pause");
-
+    sorty(1000);
+    cout << endl;
     cout << "For 10000 elements:" << endl;
-    sorty(n = 10000);
-    system("pause");
-
+    sorty(10000);
+    cout << endl;
     cout << "For 100000 elements:" << endl;
-    sorty(n = 100000);
-    system("pause");
-
+    sorty(100000);
+    cout << endl;
     cout << "For 1000000 elements:" << endl;
-    sorty(n = 1000000);
-    system("pause");
-
-    system("pause>>void");
+    sorty(1000000);
 }
