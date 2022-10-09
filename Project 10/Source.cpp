db477afb11eeb49971306361ff26e0b9484b2b0c
Kostanics <100402113+Kostanics@users.noreply.github.com>
2022-10-09 21:29:58 +0300

    Update Source.cpp

--- Project 10/Source.cpp
+++ Project 10/Source.cpp
@@ -1,48 +1,52 @@
 #include <iostream>
 using namespace std;
 
-void sorty(const int n) {
-    int x;
-    int position = 0;
-    int* arr = new int[n];
-    for (int i = 0; i < n; i++){
-        arr[i] = rand() % n; 
-        cout << arr[i] << "  ";
+struct node{
+    int e;
+    node* l, * r;
+};
+node* tree = NULL;
+
+void push(int x, node** t){
+    if ((*t) == NULL){
+        (*t) = new node;
+        (*t)->e = x;
+        (*t)->l = (*t)->r = NULL;
+        return;
     }
-    cout << endl;
-    cout << "Find: "; 
-    cin >> x;
-    if (arr[n - 1] != x){
-        arr[n - 1] = x;
-        for (; arr[position] != x; position++);
-        position++;
+    if (x > (*t)->e) {
+        push(x, &(*t)->r);
+    }
+    else {
+        push(x, &(*t)->l);
+    }
+}
+
+void print(node* t, int x)
+{
+    if (t == NULL) {
+        return;
     }
     else{
-        cout << n;
+        print(t->l, ++x);
+        for (int i = 0; i < x; ++i) {
+            cout << "_";
+        }
+        cout << t->e << endl;
+        --x;
     }
-    cout << "position: " << position;
+    print(t->r, ++x);
 }
+
 int main()
 {
-    srand(time(NULL));
-    int n;
-    cout << "For 100 elements:" << endl;
-    sorty(n = 100);
-    system("pause");
-
-    cout << "For 1000 elements:" << endl;
-    sorty(n = 1000);
-    system("pause");
-
-    cout << "For 10000 elements:" << endl;
-    sorty(n = 10000);
-    system("pause");
-
-    cout << "For 100000 elements:" << endl;
-    sorty(n = 100000);
-    system("pause");
-
-    cout << "For 1000000 elements:" << endl;
-    sorty(n = 1000000);
-    system("pause");
+    int n, e;
+    cout << "enter number:";
+    cin >> n;
+    for (int i = 0; i < n; ++i){
+        cout << "enter element:";
+        cin >> e;
+        push(e, &tree);
+    }
+    print(tree, 0);
 }
