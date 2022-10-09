9f1c6120d423e741f187a90e836ee3e558309457
Kostanics <100402113+Kostanics@users.noreply.github.com>
2022-10-09 21:19:09 +0300

    Update Source.cpp

--- Project 8/Source.cpp
+++ Project 8/Source.cpp
@@ -1,48 +1,26 @@
-#include <iostream>
+#include <iostream> 
+#include <stack>
 using namespace std;
 
-void sorty(const int n) {
-    int x;
-    int position = 0;
-    int* arr = new int[n];
-    for (int i = 0; i < n; i++){
-        arr[i] = rand() % n; 
-        cout << arr[i] << "  ";
+int main() {
+    int n, x;
+    stack<int> s;
+    cout << "enter height:";
+    cin >> n;
+    cout << endl;
+    cout << "enter point:";
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        if (i == 0) {
+            s.push(x);
+        }
+        if (i > 0) {
+            if (x > s.top()) {
+                s.push(x);
+            }
+        }
     }
     cout << endl;
-    cout << "Find: "; 
-    cin >> x;
-    if (arr[n - 1] != x){
-        arr[n - 1] = x;
-        for (; arr[position] != x; position++);
-        position++;
-    }
-    else{
-        cout << n;
-    }
-    cout << "position: " << position;
-}
-int main()
-{
-    srand(time(NULL));
-    int n;
-    cout << "For 100 elements:" << endl;
-    sorty(n = 100);
-    system("pause");
-
-    cout << "For 1000 elements:" << endl;
-    sorty(n = 1000);
-    system("pause");
-
-    cout << "For 10000 elements:" << endl;
-    sorty(n = 10000);
-    system("pause");
-
-    cout << "For 100000 elements:" << endl;
-    sorty(n = 100000);
-    system("pause");
-
-    cout << "For 1000000 elements:" << endl;
-    sorty(n = 1000000);
-    system("pause");
+    cout << "can see:";
+    cout << s.size();
 }
