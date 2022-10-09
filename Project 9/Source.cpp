680f34fe7e9a41e9c54e66139ee74d73cdb20b4f
Kostanics <100402113+Kostanics@users.noreply.github.com>
2022-10-09 21:19:40 +0300

    Update Source.cpp

--- Project 9/Source.cpp
+++ Project 9/Source.cpp
@@ -1,48 +1,69 @@
-#include <iostream>
+#include "iostream"
 using namespace std;
 
-void sorty(const int n) {
-    int x;
-    int position = 0;
-    int* arr = new int[n];
-    for (int i = 0; i < n; i++){
-        arr[i] = rand() % n; 
-        cout << arr[i] << "  ";
+int maxi(int i){
+    int max = 0;
+    while (i > 1){
+        i /= 10;
+        max++;
     }
-    cout << endl;
-    cout << "Find: "; 
-    cin >> x;
-    if (arr[n - 1] != x){
-        arr[n - 1] = x;
-        for (; arr[position] != x; position++);
-        position++;
-    }
-    else{
-        cout << n;
-    }
-    cout << "position: " << position;
+    return max;
 }
+
+int siz(int i, int r){
+    while (r > 1){
+        i /= 10;
+        r--;
+    }
+    return i % 10;
+}
+
+void sorty(int** array1, int* array, int r, int n){
+    int* arr, i, j, t = 0;
+    arr = new int[n];
+    for (i = 0; i < n; i++) {
+        arr[i] = 0;
+    }
+    for (i = 0; i < n; i++){
+        int a = siz(array[i], r);
+        array1[arr[a]][a] = array[i];
+        arr[a]++;
+    }
+    for (i = 0; i < n; i++){
+        for (j = 0; j < arr[i]; j++){
+            array[t] = array1[j][i];
+            t++;
+        }
+    }
+}
+
 int main()
 {
     srand(time(NULL));
-    int n;
-    cout << "For 100 elements:" << endl;
-    sorty(n = 100);
-    system("pause");
-
-    cout << "For 1000 elements:" << endl;
-    sorty(n = 1000);
-    system("pause");
-
-    cout << "For 10000 elements:" << endl;
-    sorty(n = 10000);
-    system("pause");
-
-    cout << "For 100000 elements:" << endl;
-    sorty(n = 100000);
-    system("pause");
-
-    cout << "For 1000000 elements:" << endl;
-    sorty(n = 1000000);
-    system("pause");
+    int n, s = 0;
+    int r, * array, ** array1;
+    cout << "enter size: ";
+    cin >> n;
+    array = new int[n];
+    cout << "elements: " << endl;
+    for (int i = 0; i < n; i++) {
+        array[i] = rand() % n;
+        cout << i + 1 << ":" << array[i] << "  ";
+    }
+    cout << endl;
+    array1 = new int* [n];
+    for (int i = 0; i < n; i++) {
+        array1[i] = new int[n];
+    }
+    for (int i = 0; i < n; i++) {
+        if (s < maxi(array[i])) {
+            s = maxi(array[i]);
+        }
+    }
+    for (r = 1; r <= s; r++) {
+        sorty(array1, array, r, n);
+    }
+    for (int i = 0; i < n; i++) {
+        cout << array[i] << " ";
+    }
 }
